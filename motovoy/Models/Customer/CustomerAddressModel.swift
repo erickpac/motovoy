ad1a718fd1c5951d48a735038dfcb98d83c27a07
Erick Pac <pakincpp@gmail.com>
2018-05-08 21:47:35 -0600

    init models (customer)

---
+++ motovoy/Models/Customer/CustomerAddressModel.swift
@@ -0,0 +1,37 @@
+//
+//  AddressModel.swift
+//  motovoy
+//
+//  Created by Erick Pac on 5/8/18.
+//  Copyright © 2018 Nextdots. All rights reserved.
+//
+
+struct CustomerAddress {
+    var id: Int?
+    var clientId: Int?
+    var name: String?
+    var customerName: String?
+    var address: String?
+    var type: String?
+    var city: String?
+    var postalCode: String?
+    var mobile: String?
+    var createdAt: String?
+    var updatedAt: String?
+    var deletedAt: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case clientId = "client_id"
+        case name
+        case customerName = "client_name"
+        case address
+        case type
+        case city
+        case postalCode = "postal_code"
+        case mobile
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case deletedAt = "deleted_at"
+    }
+}
---
+++ motovoy/Services/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  motovoy
+//
+//  Created by Erick Pac on 5/8/18.
+//  Copyright © 2018 Nextdots. All rights reserved.
+//
+
+import Foundation
