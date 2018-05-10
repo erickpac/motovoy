a5e7fc8db4b9ca7c5f67b3cc1203fe9a12fb7ff7
Erick Pac <pakincpp@gmail.com>
2018-05-09 23:35:19 -0600

    APIManager class init and create enums file for path services

--- motovoy/Helpers/Enums/ErrorCodes.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ErrorCodes.swift
-//  motovoy
-//
-//  Created by Erick Pac on 5/9/18.
-//  Copyright © 2018 Nextdots. All rights reserved.
-//
---
+++ motovoy/Helpers/Enums/UrlPath.swift
@@ -0,0 +1,15 @@
+//
+//  UrlPath.swift
+//  motovoy
+//
+//  Created by Erick Pac on 5/9/18.
+//  Copyright © 2018 Nextdots. All rights reserved.
+//
+
+enum UrlPath: String {
+    case login = "/api/auth/login"
+    case register = "/api/auth/register"
+    case logout = "/api/logout"
+    case confirmationCode = "/api/auth/confirmationCode"
+    case getConfirmationCode = "getConfirmationCode"
+}
