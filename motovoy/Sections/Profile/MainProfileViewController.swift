5287069c041f3a5d309ebcbdcd13268a16c2ec6a
Jose Quintero <jose.quintero@bairesdev.com>
2018-05-12 13:33:26 -0600

    added controllers, messages screen

---
+++ motovoy/Sections/Profile/MainProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainProfileViewController.swift
+//  motovoy
+//
+//  Created by Jose Quintero on 5/12/18.
+//  Copyright © 2018 Nextdots. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class MainProfileViewController: UIViewController {
+
+    @IBOutlet weak var container: UIView!
+    
+    var tabController: TabsController!
+
+}
